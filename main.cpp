87dea21dd4a4a8cb065dbbdab7de8f2f14332757
noble999 <nleduc2@mail.csuchico.edu>
2019-10-02 17:17:13 -0700

    new main.cpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,13 @@ int main()
   string fileName; //to get the name of the file to open
   string line; //to store a single line of a text file
   fstream fileStream; //to open a file for reading
-
+  char ans;//yes or no
+  int count=0;
+  int ccount=0;
+ do
+ {
   cout<<"What file do you want to open? ";
   getline(cin, fileName);
-  
   //STEP 1: open the fileStream for input, using the fileName specified
   fileStream.open(fileName.c_str(), ios::in);
   if(fileStream.is_open())
@@ -24,7 +27,12 @@ int main()
       {
 
         getline(fileStream,line);
+	if (!line.empty())
+	{
         cout<<line<<"\n";
+	count=count+1;
+       	ccount=ccount+line.length();
+	}
       }
       while(!(fileStream.eof()));
     // 3A: read a line from fileStream into the variable line
@@ -37,6 +45,16 @@ int main()
   {
     cout<<fileName<<" could not be opened.\n";
   }
-
+    cout<<"METADATA\n";
+    cout<<"File: "<<fileName<<"\n";
+    cout<<"Lines: "<<count<<"\n";
+    cout<<"Characters: "<<ccount<<"\n";
+    cout<<"Analyze another file (y/n)? ";
+    cin>>ans;
+    cin.ignore();
+    count=0;
+    ccount=0;
+ }
+ while(ans=='y');
   return 0;
 }
